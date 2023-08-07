8e5400900f5f7febe43f714037aafc2740445a8c
Chao Chen <alpha.1986@gmail.com>
2023-08-07 20:19:55 +0800

    add sync_build

---
+++ sync_build.sh
@@ -0,0 +1,14 @@
+source /home/work/.bashrc
+timestamp1=`stat -c %Y scripts/data.db`
+python3 scripts/strava_sync.py ${STRAVA_CLIENT_ID} ${STRAVA_CLIENT_SECRET} ${STRAVA_REFRESH_TOKEN}
+timestamp2=`stat -c %Y scripts/data.db`
+if [[ $timestamp1 == $timestamp2 ]];then
+    echo "$(date "+%Y-%m-%d %H:%M:%S") scripts/data.db not change!"
+    exit 0
+fi
+python3 scripts/gen_svg.py --from-db --title "Activity Grids" --type github --athlete ${WEB_ATHLETE} --special-distance 10 --special-distance2 20 --special-color yellow --special-color2 red --output assets/github.svg --use-localtime --min-distance 0.5
+python3 scripts/gen_svg.py --from-db --title "Over 10km Activities" --type grid --athlete ${WEB_ATHLETE}  --output assets/grid.svg --min-distance 10.0 --special-color yellow --special-color2 red --special-distance 20 --special-distance2 40 --use-localtime
+python3 scripts/gen_svg.py --from-db --type circular --use-localtime
+yarn build
+rm -rf ${RUNNING_HTML_PATH}
+mv public ${RUNNING_HTML_PATH}
