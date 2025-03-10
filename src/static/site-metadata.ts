a729c9cca9c3ecd8d6b065012e5969b161ea2023
alpha-86 <alpha.1986@gmail.com>
2025-03-10 19:04:57 +0800

    Update logo in site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -13,7 +13,7 @@ interface ISiteMetadataResult {
 const data: ISiteMetadataResult = {
   siteTitle: 'Workouts Map',
   siteUrl: 'https://ben29.xyz',
-  logo: 'https://avatars3.githubusercontent.com/u/6956444?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
+  logo: 'https://avatars.githubusercontent.com/u/4555697?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
   description: 'Personal site and blog',
   keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
   navLinks: [
