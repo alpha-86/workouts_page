d6de977c5e65cd1edb5dadf541d44b018d0c0812
alpha-86 <alpha.1986@gmail.com>
2025-03-10 19:42:01 +0800

    Update site-metadata.ts update siteTitle

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,7 +11,7 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Alpha's Workouts Map',
+  siteTitle: 'Alpha\'s Workouts Map',
   siteUrl: 'https://running.cqcy.host',
   logo: 'https://avatars.githubusercontent.com/u/4555697?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
   description: 'Personal site and blog',
